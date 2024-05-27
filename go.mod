49dc2994ce6746f40f8fc0f2813ab9a0c458c90f
uikola <ugulaev806@yandex.com>
2024-05-27 12:41:43 +0400

    Add server with POST Cloud Event handler

---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/Uikola/knative-golang
+
+go 1.22.0
+
+require (
+	github.com/cloudevents/sdk-go/v2 v2.12.0
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/rs/zerolog v1.33.0
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/Uikola/knative-golang/pkg/zlog"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	DEBUGLEVEL = 0
+)
+
+func main() {
+	log.Logger = zlog.Default(true, "dev", DEBUGLEVEL)
+
+	r := chi.NewRouter()
+	r.Post("/cloud-event", CloudEventHandler)
+
+	if err := http.ListenAndServe(":8000", r); err != nil {
+		log.Error().Err(err).Msg(err.Error())
+		os.Exit(1)
+	}
+}
+
+func CloudEventHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to read request body")
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(map[string]string{"reason": "bad request"})
+		return
+	}
+
+	event := cloudevents.NewEvent()
+
+	err = json.Unmarshal(body, &event)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to unmarshal json into CloudEvent")
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(map[string]string{"reason": "invalid CloudEvent format"})
+		return
+	}
+
+	log.Info().Msg(event.String())
+}
